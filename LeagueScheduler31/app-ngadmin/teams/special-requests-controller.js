5f0a9dfccc0612978cd7a10773162495f9651cdc
Steve Michelotti <steve.michelotti@gmail.com>
2015-01-08 15:01:30 -0500

    fixed ordering of special requests after merge with existing

--- LeagueScheduler31/app-ngadmin/teams/special-requests-controller.js
+++ LeagueScheduler31/app-ngadmin/teams/special-requests-controller.js
@@ -32,35 +32,20 @@
                 return (hour < 13 ? hour + amPm : (hour - 12) + amPm);
             });
 
-            //vm.potentialDays = data.distinctDays;
-
-            //vm.potentialDays = _.map(data.distinctDays, function (day) { return { date: day }; });
-
-            //vm.potentialDays = _.map(data.distinctDays, function (day) {
-            //    return { date: moment(day).toDate() };
-            //});
-
-            //_.forEach(vm.potentialDays, function (day) {
-            //    day.hours = _(potentialHours).map(function (hour) {
-            //        return { hour: hour };
-            //    }).value();
-            //});
-
-            //vm.scheduleRequests = _.map(data.distinctDays, function (day) {
             var emptyScheduleRequests = _.map(data.distinctDays, function (day) {
                 return {
                     date: day,
                     unavailableHours: _.map(potentialHours, function (hour) {
-                        //TODO: need to map "selected" for *existing*
                         return { hour: hour };
                     })
-                    //TODO: need to map "extraRequest" and "resolved" for *existing*
                 };
             });
 
-            //var temp = _.union(itemToEdit.scheduleRequests, emptyScheduleRequests);
             var existingRequestDays = (itemToEdit ? itemToEdit.scheduleRequests : []);
-            vm.scheduleRequests = _.uniq(_.union(existingRequestDays, emptyScheduleRequests), 'date');
+            var mergedRequests = _.uniq(_.union(existingRequestDays, emptyScheduleRequests), 'date');
+            vm.scheduleRequests = _.sortBy(mergedRequests, function (item) {
+                return moment(item.date).toDate().getTime();
+            });
             console.log('***scheduleRequests', vm.scheduleRequests);
         }
 
@@ -69,25 +54,6 @@
         }
 
         function save() {
-
-            //var schedRequests = _.chain(vm.scheduleRequests)
-            //    .filter(function (day) {
-            //        return day.extraRequest;
-            //    })
-            //    .map(function (day) {
-            //        return {
-            //            date: day.date,
-            //            //unavailableHours: _.chain(day.unavailableHours).filter('selected').map('hour').value(),
-            //            unavailableHours: day.unavailableHours,
-            //            extraRequest: day.extraRequest,
-            //            resolved: day.resolved
-            //        }
-            //    }).value();
-
-            //var schedRequests = _.filter(vm.scheduleRequests, function (day) {
-            //    return day.extraRequest || _.any(day.unavailableHours, 'selected');
-            //});
-
             var scheduleRequest = {
                 teamId: data.team.id,
                 scheduleRequests: _.filter(vm.scheduleRequests, function (day) {
